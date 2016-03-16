5d75ec1acc51a609e9e7dfacceb223ff810c6a68
Damien Lebrun <dinoboff@hotmail.com>
2016-03-16 00:38:29 +0000

    Removed bin/build (use nom script only)

--- bin/server.js
+++ bin/server.js
@@ -10,22 +10,24 @@ const serveStatic = require('serve-static');
 const www = process.argv.slice(2).pop() || path.join(__dirname, '../src');
 const cert = fs.readFileSync(path.join(__dirname, '/localhost.cert'));
 const key = fs.readFileSync(path.join(__dirname, '/localhost.key'));
+const port = process.env.npm_package_config_port || 8080;
 
 const logger = morgan('dev');
 const serve = serveStatic(www, {index: ['index.html']});
+const use = (req, resp, middlewares) => {
+  middlewares.reverse().reduce((next, handler) => {
+    return err => {
+      if (err) {
+        next(err);
+      }
 
-const server = http2.createServer({key, cert}, (req, resp) => {
-  const done = finalhandler(req, resp);
+      handler(req, resp, next);
+    };
+  }, finalhandler(req, resp))();
+};
 
-  logger(req, resp, err => {
-    if (err) {
-      done(err);
-    }
+const server = http2.createServer({key, cert}, (req, resp) => use(req, resp, [logger, serve]));
 
-    serve(req, resp, done);
-  });
-});
-
-server.listen(8080, () => console.log(
-  'Dev server listening on https://localhost:8080 (don\'t forget to use "HTTPS")'
+server.listen(port, () => console.log(
+  `Dev server listening on https://localhost:${port} (don't forget to use "HTTPS")`
 ));
